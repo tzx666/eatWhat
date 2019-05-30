72fbb921d5acee7a0c472d2db756eb8c86848562
tzx666 <916196773@qq.com>
2019-05-30 20:02:27 +0800

    update4

--- Pages/Users/Information/scoreformealScreen.js
+++ Pages/Users/Information/scoreformealScreen.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
 import {Platform, StyleSheet, Text, View,Button} from 'react-native'
 export class scoreformealScreen extends Component{
+   
     static navigationOptions = {
      //   tabBarVisible: false, // 隐藏底部导航栏
         header:null,  //隐藏顶部导航栏
       };
+      constructor(props){
+           let meals=[]
+          super(props)
+          this.state={data:meals}
+      }
+      componentDidMount(){
+          
+      }
     render(){
         return(
             <View>
--- Pages/Users/UserScreen.js
+++ Pages/Users/UserScreen.js
@@ -2,36 +2,28 @@ import React, {Component} from 'react'
 import {Platform, StyleSheet, Text, View,Button,TouchableHighlight,TextInput,Alert} from 'react-native'
 import { Avatar } from 'react-native-elements';
 import {unitWidth, width, height}from'../Adapt'
-import { gray } from 'ansi-colors';
 import { ListItem,Overlay } from 'react-native-elements'
-import { Icon } from 'react-native-elements'
-export var userinfo={name:'',password:'',email:''};
+import { ScrollView } from 'react-native-gesture-handler';
 export class UserScreen extends Component{
-    static navigationOptions = {
-      //  tabBarLabel: '我的',
-        //drawerLabel:'页面1'
-        header:null
-    };
+    static navigationOptions = {header:null};
     constructor(props){
         super(props)
-        this.state={isVisible:false,usename:'',password:'',email:''}
-          }
+        this.state={isVisible:false,usename:'',password:'',email:'',permission:-1}}
         render(){
             return(
-                <View style={style.container}>
-                   
-                        {
-                      userinfo['name']==''? <TouchableHighlight onPress={()=>this.setState({isVisible:true})}>
+                <View style={style.container}>                         
+                      {
+                        this.state.permission==-1? <TouchableHighlight onPress={()=>this.setState({isVisible:true})}>
                    <View style={style.avatar}>
                     <Avatar size="large" rounded source={{ uri:'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg', }} />
-                    <Text style={{fontSize:50*unitWidth,}}>请先登陆</Text>
+                    <Text style={{fontSize:50*unitWidth,}}>请先登陆/注册</Text>
                 </View></TouchableHighlight>
                 :<TouchableHighlight onPress={()=>this.props.navigation.navigate('MyInformation')}>
                 <View style={style.avatar}>
         <Avatar size="large" rounded source={{ uri:'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg', }} />
-        <Text style={{fontSize:50*unitWidth,}}>用户：{this.state.name}</Text>
+        <Text style={{fontSize:50*unitWidth,}}>普通用户：{this.state.name}</Text>
     </View></TouchableHighlight>
-                        }              
+                    }           
                 <View style={style.bottomborder}></View>
                  <View >
                     <ListItem style={style.list}
@@ -46,40 +38,34 @@ export class UserScreen extends Component{
                 leftIcon={{name:'person'}}
                 rightIcon={{name:'chevron-right'}}
                 bottomDivider='true'
-                onPress={() => this.props.navigation.navigate('MyInformation')}
-                />
+                onPress={() => this.props.navigation.navigate('MyInformation')}/>
                 <ListItem style={style.list}
                 title='设置'
                 leftIcon={{name:'settings'}}
                 rightIcon={{name:'chevron-right'}}
                 bottomDivider='true'
-                onPress={() => this.props.navigation.navigate('Setting')}
-                /><ListItem style={style.list}
+                onPress={() => this.props.navigation.navigate('Setting')}/>
+                <ListItem style={style.list}
                 title='关于我们'
                 leftIcon={{name:'menu'}}
                 rightIcon={{name:'chevron-right'}}
-                onPress={() => this.props.navigation.navigate('About')}
-                />
+                onPress={() => this.props.navigation.navigate('About')}/>
                 </View> 
                 <Overlay isVisible={this.state.isVisible}>
-              <TextInput  style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                  <ScrollView>
+              <TextInput autoFocus='true' style={{height: 40, borderColor: 'gray', borderWidth: 1}}
                 placeholder='input username'
         onChangeText={(name) => this.setState({name})}
         value={this.state.name}/>
-        <TextInput  style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        <TextInput autoFocus='true' style={{height: 40, borderColor: 'gray', borderWidth: 1}}
         placeholder='input password'
         onChangeText={(password) => this.setState({password})}
         value={this.state.password}/>
-        <TextInput  style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        <TextInput autoFocus='true' style={{height: 40, borderColor: 'gray', borderWidth: 1}}
         placeholder='input email'
         onChangeText={(email) => this.setState({email})}
         value={this.state.email}/>
-        <Button title='submit'onPress={()=>{
-                  userinfo['name']=this.state.name
-                  userinfo['password']=this.state.password
-                  userinfo['email']=this.state.email
-                  console.log(userinfo)
-            console.log(name)
+        <Button title='登陆'onPress={()=>{   
              fetch('http://192.168.43.40/app-contact/login.php', { 
                 method: 'post', 
                 headers: { 
@@ -88,20 +74,29 @@ export class UserScreen extends Component{
                 body: 'name='+this.state.name+'&password='+this.state.password+'&email='+this.state.email
               })
               .then(res=>res.text()) 
-              .then(function (data) { 
+              .then((data)=> { 
                 if(data==1){
-                  Alert.alert("注册成功")
+                  this.setState({isVisible:false,usename:this.state.name,password:this.state.password,email:this.state.password,permission:1})
+                  Alert.alert("登陆成功")
                 }
-                console.log(data); 
+                else if (data==-1){
+                  this.setState({isVisible:true,usename:'',password:'',email:'',permission:-1})
+                  Alert.alert("密码错误，请联系管理员")     
+                }
+                else if (data==0){
+                  this.setState({isVisible:true,usename:'',password:'',email:'',permission:-1})
+                  Alert.alert("注册失败，请联系管理员")    
+                }
               }) 
               .catch(function (error) { 
                 console.log('Request failed', error); 
-              }); 
-            this.setState({isVisible:false})}}/>
+              }
+              ); 
+           }}/>
                <Button title='back'onPress={()=>this.setState({isVisible:false})}/>
+               </ScrollView>
               </Overlay>
-                </View>
-               
+                </View>  
             );
         }
 } 
